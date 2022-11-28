6532ee728bee903373effa625f4c2afa7878b4dc
danielfcollier <danielfcollier@gmail.com>
2022-11-28 16:59:52 -0300

    Adjust formatting.

--- library/DbSheets.js
+++ library/DbSheets.js
@@ -29,66 +29,57 @@ function Create(connection, data) {
   const dbData = DbConversion_(connection, data);
   connection.sheet.appendRow(dbData);
 
-  ServiceLogger_(connection, 'Create', data.id);
+  ServiceLogger_(connection, "Create", data.id);
   return {
-    createdAt: (new Date()).toISOString(),
+    createdAt: new Date().toISOString(),
   };
 }
 
 function Read(connection, id) {
-  const {
-    sheet,
-    column,
-  } = connection;
+  const { sheet, column } = connection;
 
   const rowTarget = GetRowById_(connection, id);
   const [dbData] = sheet.getRange(rowTarget, column.start, 1, column.end).getValues();
   const data = JsonConversion_(connection, dbData);
 
-  ServiceLogger_(connection, 'Read', id);
+  ServiceLogger_(connection, "Read", id);
   return data;
 }
 
 function ReadAll(connection) {
-  const {
-    sheet,
-    row,
-    column,
-  } = connection;
+  const { sheet, row, column } = connection;
 
-  const dbAll = sheet.getRange(row.start, column.start, sheet.getMaxRows() - row.offset, column.end).getValues();
-  const dataAll = dbAll.map(data => JsonConversion_(connection, data));
+  const sheetMaxRows = sheet.getMaxRows();
+  if (sheetMaxRows - row.offset === 0) {
+    return [];
+  }
+  const dbAll = sheet.getRange(row.start, column.start, sheetMaxRows - row.offset, column.end).getValues();
+  const dataAll = dbAll.map((data) => JsonConversion_(connection, data));
 
   return dataAll;
 }
 
 function Update(connection, data) {
-  const {
-    sheet,
-    column,
-  } = connection;
+  const { sheet, column } = connection;
 
   const rowTarget = GetRowById_(connection, data.id);
   const dbData = DbConversion_(connection, data);
   sheet.getRange(rowTarget, column.start, 1, column.end).setValues([dbData]);
 
-  ServiceLogger_(connection, 'Update', data.id);
+  ServiceLogger_(connection, "Update", data.id);
   return {
-    updatedAt: (new Date()).toISOString(),
+    updatedAt: new Date().toISOString(),
   };
 }
 
 function Delete(connection, id) {
-  const {
-    sheet,
-  } = connection;
+  const { sheet } = connection;
 
   const rowTarget = GetRowById_(connection, id);
   sheet.deleteRow(rowTarget);
 
-  ServiceLogger_(connection, 'Delete', id);
+  ServiceLogger_(connection, "Delete", id);
   return {
-    deletedAt: (new Date()).toISOString(),
+    deletedAt: new Date().toISOString(),
   };
 }
-
