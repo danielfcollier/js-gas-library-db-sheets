1848d167e0eccb1bfc2e6165a0106b0ce2a4ed6f
danielfcollier <danielfcollier@gmail.com>
2023-01-22 08:08:32 -0300

    Fix tests.

--- tests/integrationE2ETest.js
+++ tests/integrationE2ETest.js
@@ -16,7 +16,7 @@ function integrationE2ETest() {
     const expected = {
       ...dbSettings,
     };
-    const { id, table, sDbSheets.specchema } = dbConnection;
+    const { id, table, schema } = dbConnection;
 
     results[testName] = Test.ExpectToBeEqual(testName, expected, { id, table, schema });
   }
